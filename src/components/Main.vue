21a3b3b05f0bccdb8dc1c955e62d124b12ef7378
Nicolò <nicocriva1@gmail.com>
2021-07-13 19:01:21 +0200

    help push

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="p-3">
-    <h5 class="text-secondary" v-if="movieTitle.length != 0">Hai cercato: {{movieTitle}}</h5>
-    <div v-if="movieTitle.length != 0" class="float-start">
+    <h5 class="text-secondary mb-4" v-if="searchText.length != 0">Hai cercato: {{searchText}}</h5>
+    <div v-if="searchText.length != 0">
+      <h5 class="text-secondary">Movies:</h5>
       <div v-for="(item, index) in movie" :key="index">
-        <div class="my-4">
+        <div class="mb-4">
+          <div><strong>Titolo: </strong>{{item.title}}</div>
+          <div><strong>Titolo originale: </strong>{{item.original_title}}</div>
+          <div><strong>Lingua: </strong>{{item.original_language}}</div>
+          <div><strong>Voto: </strong>{{item.vote_average}}</div>
+        </div>
+      </div>
+    </div>
+    <div v-if="searchText.length != 0">
+      <h5 class="text-secondary">TV Shows:</h5>
+      <div v-for="(item, index) in serie" :key="index">
+        <div class="mb-4">
           <div><strong>Titolo: </strong>{{item.title}}</div>
           <div><strong>Titolo originale: </strong>{{item.original_title}}</div>
           <div><strong>Lingua: </strong>{{item.original_language}}</div>
@@ -21,31 +33,40 @@ import axios from 'axios'
 export default {
     name: 'Main',
     props:{
-      movieTitle: String,
+      searchText: String,
     },
     data(){
       return{
-        apiURL: 'https://api.themoviedb.org/3/search/movie',
+        movieApiURL: 'https://api.themoviedb.org/3/search/movie',
+        serieApiURL: 'https://api.themoviedb.org/3/search/tv',
         apiKey: '?api_key=af93f11867caf2fb3e44dfe407851a1d',
         language: '&language=it-IT',
         query: '&query=',
-        movie: [], 
+        movie: [],
+        serie: [],
         
       }
     },
-    updated(){
-      this.getInfo();
+    created(){
+      this.getMovieInfo();
+      this.getSerieInfo();
     },
     methods: {
-      getInfo(){
-        if(this.movieTitle != ''){
-          axios
-            .get(this.apiURL + this.apiKey + this.language + this.query + this.movieTitle)
-            .then(element => {
-              this.movie = element.data.results;
-              })
+      getMovieInfo(){
+        axios
+          .get(this.movieApiURL + this.apiKey + this.language + this.query + this.searchText)
+          .then(element => {
+            this.movie = element.data.results;
+            })
 
-        }
+      },
+      getSerieInfo(){
+        axios
+          .get(this.serieApiURL + this.apiKey + this.language + this.query + this.searchText)
+          .then(element => {
+            this.serie = element.data.results;
+            })
+
       }
     }
     
