67d619b972e3cd64e636944913fc29cb1710be01
Nicolò <nicocriva1@gmail.com>
2021-07-14 12:47:16 +0200

    push milestone2

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,51 +1,55 @@
 <template>
   <div class="p-3">
-    <h5 class="text-secondary mb-4" v-if="searchText.length != 0">Hai cercato: {{searchText}}</h5>
-    
-      <h5 class="text-secondary">Movies:</h5>
-      <div v-for="movie in movies" :key="movie.id">
-        <div class="mb-4">
-          <div>
-            <strong>Titolo: </strong>
-            {{movie.title}}
+    <h5 class="text-secondary">Movies:</h5>
+    <div v-for="movie in movies" :key="movie.id">
+      <div class="mb-4">
+        <div>
+          <strong>Titolo: </strong> {{movie.title}}
+        </div>
+        <div>
+          <strong>Titolo originale: </strong> {{movie.original_title}}
+        </div>
+        <div>
+          <strong>Voto: </strong> {{movie.vote_average}}
+        </div>
+        <div>
+          <div v-if="flags.includes(movie.original_language)">
+            <img class="language-img" 
+            :src=" 
+                require(`../assets/${movie.original_language}.png`)
+                 "
+            :alt="movie.original_language">
           </div>
-          <div>
-            <strong>Titolo originale: </strong>
-            {{movie.original_title}}
-          </div>
-          <div>
-            <strong>Lingua: </strong>
-            {{movie.original_language}}
-          </div>
-          <div>
-            <strong>Voto: </strong>
-            {{movie.vote_average}}
-          </div>
+          <div v-else><strong>Lingua: </strong> {{movie.original_language}}</div>
         </div>
       </div>
+    </div>
     
     
-      <h5 class="text-secondary">TV Shows:</h5>
-      <div v-for="serie in series" :key="serie.id">
-        <div class="mb-4">
-          <div>
-            <strong>Titolo: </strong>
-            {{serie.name}}
+    <h5 class="text-secondary">TV Shows:</h5>
+    <div v-for="serie in series" :key="serie.id">
+      <div class="mb-4">
+        <div>
+          <strong>Titolo: </strong> {{serie.name}}
+        </div>
+        <div>
+          <strong>Titolo originale: </strong> {{serie.original_name}}
+        </div>
+        <div>
+          <strong>Voto: </strong> {{serie.vote_average}}
+        </div>
+        <div>
+          <div v-if="flags.includes(serie.original_language)">
+            <img class="language-img"
+            :src="
+                  require(`../assets/${serie.original_language}.png`)
+                 " 
+            :alt="serie.original_language">
           </div>
-          <div>
-            <strong>Titolo originale: </strong>
-            {{serie.original_name}}
-          </div>
-          <div>
-            <strong>Lingua: </strong>
-            {{serie.original_language}}
-          </div>
-          <div>
-            <strong>Voto: </strong>
-            {{serie.vote_average}}
-          </div>
+          <div v-else><strong>Lingua: </strong> {{serie.original_language}}</div>
         </div>
       </div>
+    </div>
    
   </div>
 </template>
@@ -58,11 +62,18 @@ export default {
       movies: Array,
       series: Array,
       searchText: String,
+    },
+    data(){
+      return {
+        flags: ['en', 'it', 'es', 'de', 'ja', 'fr']
+      }
     }
     
 }
 </script>
 
 <style lang="scss" scoped>
-
+.language-img{
+  width: 30px;
+}
 </style>
